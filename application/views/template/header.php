d5cd80155ebec404318c407bd16ad8e2a1ca77f0
Riwan3 <104562592+Riwan3@users.noreply.github.com>
2024-12-23 18:40:24 +0700

    Update header.php

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -27,7 +27,7 @@ header.php
 		<!-- ============================================================== -->
 		<div class="dashboard-header">
 			<nav class="navbar navbar-expand-lg bg-white fixed-top">
-				<a class="navbar-brand" href="<?= base_url() ?>">SIMDAWA-FERIGUN APP</a>
+				<a class="navbar-brand" href="<?= base_url() ?>">SIMDAWA</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
