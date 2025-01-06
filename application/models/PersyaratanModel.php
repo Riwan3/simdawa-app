b905b2809eed7524940d8d5b7c4826202e724ea0
Riwan3 <104562592+Riwan3@users.noreply.github.com>
2025-01-06 17:19:04 +0700

    Membuat CRU Persyaratan

---
+++ application/models/PersyaratanModel.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class PersyaratanModel extends CI_Model
+{
+	private $table = 'persyaratan';
+
+	public function get_all()
+	{
+		return $this->db->get($this->table)->result();
+	}
+
+	public function insert()
+	{
+		$data = [
+			'nama_persyaratan' => $this->input->post('nama_persyaratan'),
+			'keterangan' => $this->input->post('keterangan'),
+		];
+		$this->db->insert($this->table, $data);
+	}
+
+	public function update()
+	{
+		$data = [
+			'nama_persyaratan' => $this->input->post('nama_persyaratan'),
+			'keterangan' => $this->input->post('keterangan'),
+		];
+		$this->db->where('id', $this->input->post('id'));
+		$this->db->update($this->table, $data);
+	}
+
+	public function get_by_id($id)
+	{
+		return $this->db->get_where($this->table, ['id' => $id])->row();
+	}
+
+	public function delete($id)
+	{
+		$this->db->where('id', $id);
+		$this->db->delete($this->table);
+	}
+}
